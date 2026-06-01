c71ee0a98790c0916db9a77a43e4d704b98cc58e
nickinn-ttw <drnickolascruz@gmail.com>
2026-05-31 22:49:28 -0300

    Ajustes Gerais e Correção de Bugs

--- atmos-store/src/App.jsx
+++ atmos-store/src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Store from './components/Store/Store';
-import Library from './components/Library/Library'; 
-import GameDetails from "./components/GameDetails/GameDetails.jsx"; // NOVO: Importando a página de detalhes
+import Library from './components/Library/Library';
+import GameDetails from './components/GameDetails/GameDetails.jsx';
+import CreateGame from './components/CreateGame/CreateGame.jsx';
 
-// Componente que protege rotas privadas
 function RotaProtegida({ children }) {
   const token = localStorage.getItem('atmos_token');
-  if (!token) {
-    return <Navigate to="/" replace />;
-  }
+  if (!token) return <Navigate to="/" replace />;
   return children;
 }
 
@@ -18,38 +16,23 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Rota inicial (Login) */}
         <Route path="/" element={<Login />} />
 
-        {/* Rota da Loja - só entra com token */}
-        <Route
-          path="/store"
-          element={
-            <RotaProtegida>
-              <Store />
-            </RotaProtegida>
-          }
-        />
+        <Route path="/store" element={
+          <RotaProtegida><Store /></RotaProtegida>
+        } />
 
-        {/* Rota privada da biblioteca */}
-        <Route
-          path="/library"
-          element={
-            <RotaProtegida>
-              <Library />
-            </RotaProtegida>
-          }
-        />
+        <Route path="/library" element={
+          <RotaProtegida><Library /></RotaProtegida>
+        } />
 
-        {/* NOVO: Rota privada para Detalhes do Jogo usando o ID dinâmico */}
-        <Route
-          path="/game/:id"
-          element={
-            <RotaProtegida>
-              <GameDetails />
-            </RotaProtegida>
-          }
-        />
+        <Route path="/game/:id" element={
+          <RotaProtegida><GameDetails /></RotaProtegida>
+        } />
+
+        <Route path="/create-game" element={
+          <RotaProtegida><CreateGame /></RotaProtegida>
+        } />
       </Routes>
     </Router>
   );
